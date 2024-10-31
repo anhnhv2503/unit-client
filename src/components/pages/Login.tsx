496db18a312cbbc1ba4da6c958cac9b6a6acb36e
thienhung27hao@gmail.com <thienhung27hao@gmail.com>
2024-10-31 11:51:01 +0700

    feat(register): add register page

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -2,15 +2,17 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useDocumentTitle } from "@uidotdev/usehooks";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   useDocumentTitle("Sign In");
+  const nav = useNavigate();
 
   return (
     <div
-      className={`flex items-center justify-center min-h-96 p-8 pb-44 bg-zinc-950  h-screen`}
+      className={`flex min-h-full flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8 bg-zinc-950 h-screen`}
     >
-      <div className="w-full max-w-md p-8 space-y-4 bg-white shadow-md rounded-lg">
+      <div className="w-full max-w-md p-8 space-y-4 bg-white shadow-md rounded-lg ">
         <h2 className="text-center text-3xl font-extrabold text-gray-900">
           SIGN IN
         </h2>
@@ -49,6 +51,17 @@ const Login = () => {
           <div>
             <Button className="w-full p-6">Sign In</Button>
           </div>
+          <p className="text-center">
+            Not a member? {""}
+            <a
+              className="cursor-pointer"
+              onClick={() => {
+                nav("/register");
+              }}
+            >
+              Create an account{" "}
+            </a>
+          </p>
         </div>
       </div>
     </div>
