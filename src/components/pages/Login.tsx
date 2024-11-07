d1d9e1d4d0146672d5559e88bf7df6a3b9a0d1c3
thienhung27hao@gmail.com <thienhung27hao@gmail.com>
2024-11-07 16:21:13 +0700

    feat(login): implement validate input from login form

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,13 +1,35 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { LoginBody, LoginBodyType } from "@/schema/auth.schema";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { useDocumentTitle } from "@uidotdev/usehooks";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const Login = () => {
   useDocumentTitle("Sign In");
   const nav = useNavigate();
+
+  const {
+    register: login,
+    handleSubmit,
+    setError,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginBodyType>({
+    resolver: zodResolver(LoginBody),
+  });
+
+  const onSubmit: SubmitHandler<LoginBodyType> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      reset();
+    } catch (error) {
+      setError("root", { message: "Error" });
+    }
+  };
 
   return (
     <div
@@ -26,42 +48,82 @@ const Login = () => {
           SIGN IN
         </h2>
         <div className="space-y-3">
-          <div>
-            <Label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </Label>
-            <Input
-              id="email"
-              type="email"
-              className="w-full mt-1 p-6 input input-bordered bg-white text-black border-gray-300"
-              placeholder="Enter your email"
-              required
-            />
-          </div>
-          <div>
-            <Label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </Label>
-            <Input
-              id="password"
-              type="password"
-              className="w-full mt-1 p-6 input input-bordered bg-white text-black border-gray-300"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
+          <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <Label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </Label>
+              <Input
+                {...login("email")}
+                id="email"
+                type="email"
+                className="w-full mt-1 p-6 input input-bordered bg-white text-black border-gray-300"
+                placeholder="Enter your email"
+              />
+              {errors.email && (
+                <div className=" mt-0 text-red-500 ">
+                  {errors.email.message}
+                </div>
+              )}
+            </div>
 
-          <div>
-            <Button className="w-full p-6 dark:bg-black dark:text-white dark:hover:bg-zinc-500">
-              Sign In
-            </Button>
-          </div>
+            <div>
+              <Label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </Label>
+              <Input
+                {...login("password")}
+                id="password"
+                type="password"
+                className="w-full mt-1 p-6 input input-bordered bg-white text-black border-gray-300"
+                placeholder="Enter your password"
+              />
+              {errors.password && (
+                <div className="text-red-500">{errors.password.message}</div>
+              )}
+            </div>
+
+            <div>
+              {isSubmitting ? (
+                <>
+                  <div className="flex justify-center my-5">
+                    <svg
+                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-black"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <Button className=" w-full p-6 dark:bg-black dark:text-white dark:hover:bg-zinc-500">
+                    Sign In
+                  </Button>
+                </>
+              )}
+            </div>
+          </form>
           <p className="text-center dark:text-black">
             Not a member? {""}
             <a
