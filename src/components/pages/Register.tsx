b03d9b7c11d466185063c3989985f74a81b82162
anhnhv2503 <nguyenhavietanh2503@gmail.com>
2024-11-07 15:55:54 +0700

    Fix: modified register form

--- src/components/pages/Register.tsx
+++ src/components/pages/Register.tsx
@@ -1,13 +1,39 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { RegisterBody, RegisterBodyType } from "@/schema/auth.schema";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useDocumentTitle } from "@uidotdev/usehooks";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import { Label } from "../ui/label";
 
 const Register = () => {
   useDocumentTitle("Sign Up");
   const nav = useNavigate();
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const form = useForm<RegisterBodyType>({
+    resolver: zodResolver(RegisterBody),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  function onSubmit(values: RegisterBodyType) {
+    console.log(values);
+  }
   return (
     <div
       className={`flex min-h-full flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8 bg-gradient-to-r from-purple-500 to-pink-500 h-screen`}
@@ -25,58 +51,75 @@ const Register = () => {
           SIGN UP
         </h2>
         <div className="space-y-3">
-          <div>
-            <Label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </Label>
-            <Input
-              id="email"
-              type="email"
-              className="w-full mt-1 p-6 input input-bordered bg-white text-black border-gray-300"
-              placeholder="Enter your email"
-              required
-            />
-          </div>
-          <div>
-            <Label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </Label>
-            <Input
-              id="password"
-              type="password"
-              className="w-full mt-1 p-6 input input-bordered bg-white text-black border-gray-300"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-
-          <div>
-            <Label
-              htmlFor="confirm-password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Confirm Password
-            </Label>
-            <Input
-              id="confirm-password"
-              type="password"
-              className="w-full mt-1 p-6 input input-bordered bg-white text-black border-gray-300"
-              placeholder="Enter your confirm-password"
-              required
-            />
-          </div>
-
-          <div>
-            <Button className="w-full p-6 dark:bg-black dark:text-white dark:hover:bg-zinc-500">
-              Sign Up
-            </Button>
-          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="dark:text-black">Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="dark:text-black">Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Password"
+                        type={showPassword ? "text" : "password"}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="dark:text-black">
+                      Confirm Password
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Confirm Password"
+                        type={showPassword ? "text" : "password"}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex items-center space-x-2">
+                <Checkbox
+                  id="showPassword"
+                  className="w-4 h-4 data-[state=checked]:bg-zinc-900 data-[state=checked]:border-zinc-500"
+                  onCheckedChange={() => setShowPassword(!showPassword)}
+                />
+                <label
+                  htmlFor="showPassword"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Show Password
+                </label>
+              </div>
+              <Button className="w-full p-6 dark:bg-black dark:text-white dark:hover:bg-zinc-500">
+                Sign Up
+              </Button>
+            </form>
+          </Form>
 
           <p className="text-center dark:text-black">
             Already have account ? {""}
